e826f5fdd195ca292b4cfee13a2087620fbb1672
Nemesis <lorenzo.balugani@gmail.com>
2022-11-30 20:41:55 +0100

    :sparkles: Items delivery

--- balocco/server/routes/api/giveaway/v1/router.py
+++ balocco/server/routes/api/giveaway/v1/router.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 import sqlalchemy.exc
 from pydantic import typing
@@ -69,8 +70,42 @@ async def close_giveaway(giveaway: tables.Giveaway = fastapi.Depends(deps.dep_gi
 async def join_giveaway(giveaway: tables.Giveaway = fastapi.Depends(deps.dep_giveaway),
                         current_user: tables.User = fastapi.Depends(deps.dep_user),
                         session: Session = fastapi.Depends(deps.dep_session)):
+    time = datetime.datetime.now()
+    if time > giveaway.closing_date:
+        raise ResourceNotFound
     try:
         crud.quick_create(session, tables.Signup(user_id=current_user.id, giveaway_id=giveaway.id))
     except sqlalchemy.exc.IntegrityError:
         raise CompulsiveJoin
+    return giveaway
+
+
+@router.put("/provide/{giveaway_id}", dependencies=[Depends(auth.implicit_scheme)],
+            response_model=models.full.GiveawayFull)
+async def provide_items(giveaway: tables.Giveaway = fastapi.Depends(deps.dep_giveaway),
+                        current_user: tables.User = fastapi.Depends(deps.dep_user),
+                        session: Session = fastapi.Depends(deps.dep_session)):
+    if not giveaway.active:
+        raise ResourceNotFound
+    subscribed_users: list[tables.User] = [entry.user for entry in giveaway.signups]
+    random.shuffle(subscribed_users)
+    items: list[tables.Item] = [item for item in giveaway.items if item.obtainable]
+    random.shuffle(items)
+    max_val = len(subscribed_users)
+    mode = "users"
+    if max_val > len(items):
+        mode = "items"
+        max_val = len(items)
+    j = 0
+    for i in range(0, max_val, 1):
+        if mode == "items":
+            items[i].winner_id = subscribed_users[j].id
+            items[i].obtainable = False
+            session.commit()
+            j += 1
+        else:
+            items[j].winner_id = subscribed_users[i].id
+            items[j].obtainable = False
+            session.commit()
+            j += 1
     return giveaway
