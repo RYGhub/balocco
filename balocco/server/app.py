0846c8d2a2605c3a4eb60472fe2fd58a2bf3ca8f
Nemesis <lorenzo.balugani@gmail.com>
2022-12-12 19:08:36 +0100

    :sparkles: Small changes

--- balocco/server/app.py
+++ balocco/server/app.py
@@ -23,17 +23,7 @@ app = fastapi.FastAPI(
     version="0.1",
 )
 
-origins = ["http://127.0.0.1:3000"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-origins = ["https://navigator.erre2.fermitech.info", "http://127.0.0.1:3000"]
+origins = ["https://balocco-navi.fermitech.info"]
 
 app.add_middleware(
     CORSMiddleware,
