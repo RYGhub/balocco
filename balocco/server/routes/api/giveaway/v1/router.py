ea003707a8b6a49a56942701f0fde70b07032a8e
Stefano Pigozzi <me@steffo.eu>
2022-12-23 13:36:43 +0100

    Randomize seed after sending an item
    This prevents a single user getting all 0-value items

--- balocco/server/routes/api/giveaway/v1/router.py
+++ balocco/server/routes/api/giveaway/v1/router.py
@@ -131,6 +131,7 @@ async def provide_items(giveaway: tables.Giveaway = fastapi.Depends(deps.dep_giv
         user: GiveawayPartecipant = partecipants.pop(0)
         # It's a match!
         user.current_value += item.value
+        user.seed = random.random()
         item.winner = user.user
         item.obtainable = False
         # Readd the user to the partecipants
